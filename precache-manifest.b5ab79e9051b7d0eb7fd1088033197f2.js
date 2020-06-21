2e425b34d536781f6cf38631e8704f39c705a659
unknown <you@example.com>
2020-06-21 23:02:26 +0300

    Updates

---
+++ precache-manifest.b5ab79e9051b7d0eb7fd1088033197f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80aff9de412a8dc71590a27f10388884",
+    "url": "/goit-react-hw-02-feedback1/index.html"
+  },
+  {
+    "revision": "2cf7c709768b95a58681",
+    "url": "/goit-react-hw-02-feedback1/static/js/2.f0b0019f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback1/static/js/2.f0b0019f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f568b4ec5b7f15c9af76",
+    "url": "/goit-react-hw-02-feedback1/static/js/main.748c316a.chunk.js"
+  },
+  {
+    "revision": "e9197a885788ca1a796e",
+    "url": "/goit-react-hw-02-feedback1/static/js/runtime-main.5a1998c5.js"
+  }
+]);
